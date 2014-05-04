7b955556ffc66b09399fa5feafc146cf37b2d6d4
mikio <mikio@192.168.10.10>
2014-05-04 12:25:07 +0900

    サンプルコミット

---
+++ TESTA/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+
+	}
+
+}
